e8257eff50f657beebe653dfd32b3d8451481fa9
Paolo de Rosa <paolo.de.rosa@gmail.com>
2017-12-29 15:41:07 +0100

    Update disqus.js

--- sphinxcontrib/_static/disqus.js
+++ sphinxcontrib/_static/disqus.js
@@ -1,10 +1,14 @@
 var disqus_shortname;
 var disqus_identifier;
+var disqus_config;
+
 (function() {{
     var disqus_thread = $("#disqus_thread");
     disqus_shortname = disqus_thread.data('disqus-shortname');
     disqus_identifier = disqus_thread.data('disqus-identifier');
     var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
     dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+    dsq.setAttribute('data-timestamp', +new Date());
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
 }})();
+
